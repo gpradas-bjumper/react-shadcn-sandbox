c0ad553e5cd5d6ae67f8cf17a510c2bcc8b8c039
Guillermo P. <gpradas@bjumper.com>
2025-03-20 09:22:22 +0100

    Implement vitest and testing library to playground

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test : {
+    globals: true,
+    environment: 'happy-dom'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 })
